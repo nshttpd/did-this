1cd0ab6b16a28738cf872b7b9363bbb547e5f4d3
Steve Brunton <brunton@dudeman.com>
2018-07-27 23:29:13 -0400

    more start of things

--- cmd/root.go
+++ cmd/root.go
@@ -33,26 +33,34 @@ import (
 	"fmt"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
-var cfgFile string
+const (
+	DEFAULT_CONFIG_FILE = ".didthis/config.json"
+)
+
+var (
+	cfgFile  string
+	logLevel string
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "didthis",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "task completion tracking app",
+	Long: `A command line task completion tracking app. Added completed
+tasks will end up in a bucket based on the date they are added. All tasks
+can be listed back out for daily next day reporting.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	didthis add "this is something I did today"
+	didthis list
+	didthis yesterday
+
+`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -65,40 +73,13 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.didthis.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".didthis" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".didthis")
+	hd, err := homedir.Dir()
+	if err != nil {
+		log.Fatalf("error discerning homedir : %v", err)
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	cfgFile = fmt.Sprintf("%s/%s", hd, DEFAULT_CONFIG_FILE)
 
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", cfgFile, "config file")
+	rootCmd.PersistentFlags().StringVar(&logLevel, "loglevel", "info", "log level")
 }
